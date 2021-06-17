0b6eb4c81ae0d01f25929d91af8fe8f6ef0b2d7c
WUYUEZONG <w.yzong@outlook.com>
2021-06-17 14:38:07 +0800

    init

---
+++ LearnAboutOCDemo/Home/HomeTableViewController.m
@@ -0,0 +1,31 @@
+//
+//  HomeTableViewController.m
+//  LearnAboutOCDemo
+//
+//  Created by GINOKEO on 2021/6/17.
+//
+
+#import "HomeTableViewController.h"
+
+@interface HomeTableViewController ()
+
+@end
+
+@implementation HomeTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
