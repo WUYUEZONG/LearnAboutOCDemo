99bf2291cbedea05981741d6d92e15d9af29368d
WUYUEZONG <w.yzong@outlook.com>
2021-06-17 14:29:03 +0800

    Initial Commit

---
+++ LearnAboutOCDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LearnAboutOCDemo
+//
+//  Created by GINOKEO on 2021/6/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LearnAboutOCDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  LearnAboutOCDemo
+//
+//  Created by GINOKEO on 2021/6/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
